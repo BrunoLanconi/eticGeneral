7a726c5c1428f14ac38dee3effbfc5d1d1bd82fc
Bruno Lançoni <services@brunolanconi.com.br>
2023-12-30 15:42:54 +0000

    refactor: cleaning.

--- samples/MDN/Example 4: Using Stylesheets/javascript.js
+++
@@ -1,7 +0,0 @@
-const ss = document.styleSheets;
-
-for (let i = 0; i < ss.length; i++) {
-  for (let j = 0; j < ss[i].cssRules.length; j++) {
-    dump(`${ss[i].cssRules[j].selectorText}\n`);
-  }
-}
