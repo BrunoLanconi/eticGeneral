faf545c4e8766860f146a43038e2373dec2e5a51
Bruno Lançoni <services@brunolanconi.com.br>
2024-03-13 21:58:53 +0000

    feat: add .gitignore and new API endpoint for getting bird data

--- Docker/project/api/src/router.py
+++ Docker/project/api/src/router.py
@@ -15,3 +15,24 @@ async def get_call(_: Request):
 @api_router.post("/")
 async def post_call(_: Request):
     return {"message": "You successfully POST called the API!"}
+
+
+@api_router.get("/birds")
+async def get_birds(_: Request):
+    return [
+        {
+            "name": "Eagle",
+            "data": {"color": "Brown", "age": 5, "weight": 5.5},
+            "img": "https://cdn.pixabay.com/photo/2014/11/29/19/33/bald-eagle-550804_1280.jpg",
+        },
+        {
+            "name": "Sparrow",
+            "data": {"color": "Brown", "age": 1, "weight": 0.5},
+            "img": "https://cdn.pixabay.com/photo/2018/12/25/20/11/sparrow-3894775_1280.jpg",
+        },
+        {
+            "name": "Parrot",
+            "data": {"color": "Green", "age": 2, "weight": 0.7},
+            "img": "https://cdn.pixabay.com/photo/2017/03/12/23/31/kea-2138420_960_720.jpg",
+        },
+    ]
